6b11a5eeb8cb21495c7cdae8cb2a8fd706fa7080
firengizz099 <121484651+firengizz099@users.noreply.github.com>
2023-08-29 13:48:31 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -24,7 +24,7 @@ saat = pygame.time.Clock()
 # Yılan ve yiyecek başlangıç koordinatları
 yilan_x = ekran_genislik / 2
 yilan_y = ekran_yukseklik / 2
-yilan_hizi = 10
+yilan_hizi = 3
 yilan_genislik = 15
 yilan_yukseklik = 15
 
@@ -35,15 +35,24 @@ yiyecek_y = round(random.randrange(0, ekran_yukseklik - yilan_yukseklik) / 10.0)
 yilan_parcalari = []
 yilan_boyu = 1
 
+# Yavaş büyüme için gerekli değişkenler
+yavaşlama_sayacı = 0
+yavaşlama_sınırı = 10
+
+# Yön değiştirme için gerekli değişkenler
+yon_x = yilan_hizi
+yon_y = 0
+
 # Ana döngü
 oyun_bitti = False
 oyun_devam = True
+yeme_kaydedildi = False  # Yemi yediyse True
 while oyun_devam:
 
     while oyun_bitti:
         ekran.fill(beyaz)
         font = pygame.font.SysFont(None, 50)
-        mesaj = font.render("Oyun Bitti! Yeniden başlamak için R tuşuna basın. Çıkış yapmak için Q tuşuna basın.", True, siyah)
+        mesaj = font.render("Game Over! Press R to restart.", True, siyah)
         ekran.blit(mesaj, (ekran_genislik / 6, ekran_yukseklik / 2))
         pygame.display.update()
 
@@ -66,51 +75,47 @@ while oyun_devam:
         if event.type == pygame.QUIT:
             oyun_devam = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                yilan_hizi = -10
-                yilan_y = 0
-            elif event.key == pygame.K_RIGHT:
-                yilan_hizi = 10
-                yilan_y = 0
-            elif event.key == pygame.K_UP:
-                yilan_hizi = 0
-                yilan_y -= 10  # Yılanı yukarı yönde hareket ettir
-            elif event.key == pygame.K_DOWN:
-                yilan_hizi = 0
-                yilan_y += 10  # Yılanı aşağı yönde hareket ettir
-    if yilan_x == yiyecek_x and yilan_y == yiyecek_y:
-        yiyecek_x = round(random.randrange(0, ekran_genislik - yilan_genislik) / 10.0) * 10.0
-        yiyecek_y = round(random.randrange(0, ekran_yukseklik - yilan_yukseklik) / 10.0) * 10.0
-        yilan_boyu += 1
+            if event.key == pygame.K_LEFT and yon_x != yilan_hizi:
+                yon_x = -yilan_hizi
+                yon_y = 0
+            elif event.key == pygame.K_RIGHT and yon_x != -yilan_hizi:
+                yon_x = yilan_hizi
+                yon_y = 0
+            elif event.key == pygame.K_UP and yon_y != yilan_hizi:
+                yon_x = 0
+                yon_y = -yilan_hizi
+            elif event.key == pygame.K_DOWN and yon_y != -yilan_hizi:
+                yon_x = 0
+                yon_y = yilan_hizi
+            elif event.key == pygame.K_RETURN:
+                if yilan_x == yiyecek_x and yilan_y == yiyecek_y and not yeme_kaydedildi:
+                    yilan_boyu += 1
+                    yavaşlama_sayacı = 0
+                    yeme_kaydedildi = False
 
+    if yilan_x == yiyecek_x and yilan_y == yiyecek_y and not yeme_kaydedildi:
+        yeme_gorev = True
+        yeme_kaydedildi = True
 
     if yilan_x >= ekran_genislik or yilan_x < 0 or yilan_y >= ekran_yukseklik or yilan_y < 0:
         oyun_bitti = True
 
-    yilan_x += yilan_hizi
+    yilan_x += yon_x
+    yilan_y += yon_y
     ekran.fill(beyaz)
 
-    pygame.draw.rect(ekran, yesil, [yiyecek_x, yiyecek_y, yilan_genislik, yilan_yukseklik])
-    yilan_bas = []
-    yilan_bas.append(yilan_x)
-    yilan_bas.append(yilan_y)
-    yilan_parcalari.append(yilan_bas)
-    if len(yilan_parcalari) > yilan_boyu:
-        del yilan_parcalari[0]
-
-    for parca in yilan_parcalari[:-1]:
-        if parca == yilan_bas:
-            oyun_bitti = True
+    if not yeme_kaydedildi:
+        pygame.draw.rect(ekran, yesil, [yiyecek_x, yiyecek_y, yilan_genislik, yilan_yukseklik])
+    else:
+        yavaşlama_sayacı += 1
+        if yavaşlama_sayacı >= yavaşlama_sınırı:
+            yavaşlama_sayacı = 0
+            yeme_kaydedildi = False
 
     pygame.draw.rect(ekran, siyah, [yilan_x, yilan_y, yilan_genislik, yilan_yukseklik])
 
     saat.tick(30)
     pygame.display.update()
-
-    if yilan_x == yiyecek_x and yilan_y == yiyecek_y:
-        yiyecek_x = round(random.randrange(0, ekran_genislik - yilan_genislik) / 10.0) * 10.0
-        yiyecek_y = round(random.randrange(0, ekran_yukseklik - yilan_yukseklik) / 10.0) * 10.0
-        yilan_boyu += 1
 
 pygame.quit()
 quit()
